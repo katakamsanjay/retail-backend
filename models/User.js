abf6b3edc835ac51f96480125d290f886bde13ae
katakamsanjay <katakamsanjay49@gmail.com>
2025-10-11 12:23:40 +0530

    Initial backend commit

--- models/User.js
+++ models/User.js
@@ -1,23 +1,15 @@
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  password: { type: String, required: true }, // plain text
   role: { type: String, enum: ["admin", "staff"], default: "staff" },
 }, { timestamps: true });
 
-// Hash password before saving
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
-  this.password = await bcrypt.hash(this.password, 10);
-  next();
-});
-
-// Compare password
+// Compare password (plain text)
 userSchema.methods.comparePassword = async function (password) {
-  return bcrypt.compare(password, this.password);
+  return password === this.password;
 };
 
 module.exports = mongoose.model("User", userSchema);
